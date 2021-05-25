ff07f036761d4a34f20b15e8ca11cd7f7d6824a1
bill <billbsing@gmail.com>
2021-05-25 10:46:08 +0800

    cleanup tests

--- tests/test1.c
+++ tests/test1.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include <convex.h>
 
-int main (int argc, char *argv) {
+int main() {
 	convex_p convex;
-	if (!convex_init(&convex)) {
-		printf("failed to load convex");
+	int result = convex_init(&convex);
+	if (result) {
+		printf("failed to load conve %d\n", result);
 		return 2;
 	}
 	convex_close(convex);
